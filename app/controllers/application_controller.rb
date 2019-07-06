44946ec43140b714a1157e98240c7b90d6b1de5f
Joshua Crum <joshua.crum@gmail.com>
2019-07-06 21:36:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,12 +25,12 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  get '/recipes/:id/edit' do 
+  get '/recipes/:id/edit' do
     @recipe = Recipe.find_by_id(params[:id])
-    erb :edit 
+    erb :edit
   end
 
-  patch '/recipes/:id' do  
+  patch '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
     @recipe.name = params[:name]
     @recipe.ingredients = params[:ingredients]
