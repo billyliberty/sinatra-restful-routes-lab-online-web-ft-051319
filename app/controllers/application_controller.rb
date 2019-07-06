107e437e3c8645f92cdfe2404cb0f5b68de80547
Joshua Crum <joshua.crum@gmail.com>
2019-07-06 21:29:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,23 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  configure do 
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/recipes/new' do 
+    erb :new
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
 end
